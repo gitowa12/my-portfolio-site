5b2c012b56605d0bd40fcb469e0113fcb6b6c8a7
gitowa12 <118157881+gitowa12@users.noreply.github.com>
2024-05-23 17:10:21 +0900

    fonto変更

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -9,14 +9,14 @@ const Profile = () => {
         </h2>
         <div className=" flex flex-col justify-center  items-center md:flex-row">
           <img
-            className="w-[120px]  rounded-full bg-slate-200 mb-[16px] md:w-[160px] md:mr-[24px] md:mb-0"
+            className="w-[120px] rounded-full bg-slate-200 mb-[16px] md:w-[160px] md:mr-[24px] md:mb-0"
             src="/僕のアイコン画像400x400.jpg"
             alt="icon"
           />
 
           <div className="flex flex-col items-center md:items-start">
             <p className="font-bold text-[20px] mb-[16px] md:text-[28px] md:mb-0">towa</p>
-            <p className=" text-[18px] md:text-[20px] md:max-w-[480px] ">
+            <p className=" text-[18px] font-medium md:text-[20px] md:max-w-[480px] ">
               フロントエンドエンジニア。
               <br />
               1999年生まれ。沖縄県出身、東京都在住。大学時代に上京し、新卒で入社した会社でフロントエンドエンジニアとしてのキャリアをスタート。
